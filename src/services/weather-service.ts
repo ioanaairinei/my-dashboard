20dff246786d3662eb53d34b9aea40df16a21838
ioanaairinei <94375180+ioanaairinei@users.noreply.github.com>
2023-05-29 20:54:43 +0300

    update weather-service.ts

--- src/services/weather-service.ts
+++ src/services/weather-service.ts
@@ -2,7 +2,7 @@ export const getRealTimeWeather = async (
   city: string = "Cluj",
   noOfDays: number = 3
 ) => {
-  const url = `http://api.weatherapi.com/v1/current.json?key=129c4e38b14f48959fb173008232705&q=${city}&aqi=no`;
+  const url = `http://api.weatherapi.com/v1/current.json?key=N/A&q=${city}&aqi=no`;
   const options = {
     method: "GET",
   };
