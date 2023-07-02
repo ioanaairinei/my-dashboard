523c9aa12119d991eda52f3acfaf83003719af53
ioanaairinei <ioana.airinei87@gmail.com>
2023-07-02 13:58:18 +0300

    memoize cards container

--- src/components/cards-container/CardsContainer.tsx
+++ src/components/cards-container/CardsContainer.tsx
@@ -2,13 +2,14 @@ import PhotoCard from '../photo-card/PhotoCard';
 import './cards-container.less';
 import { type ImagesKey } from '../../assets/images/paintings/Images';
 import { type Photo } from '../../views/paintings/Paintings';
+import React from 'react';
 
 interface CardsContainerProps {
   onCardClick: (id: number) => void;
   photos: Photo[];
 }
 
-function CardsContainer({ onCardClick, photos }: CardsContainerProps) {
+const CardsContainer = React.memo(({ onCardClick, photos }: CardsContainerProps) => {
   return (
     <div className="cards-container-class" id="cards-container">
       {photos.map((photo) => (
@@ -23,6 +24,6 @@ function CardsContainer({ onCardClick, photos }: CardsContainerProps) {
       ))}
     </div>
   );
-}
+});
 
 export default CardsContainer;
